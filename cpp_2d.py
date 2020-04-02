67b1ce8f092ca37db5551a71c35bb3ceba53d6d9
m4rquee <lucasxm12@gmail.com>
2020-04-02 20:29:13 -0300

    O(nlog(n)) solution

--- cpp_2d.py
+++ cpp_2d.py
@@ -1,53 +1,63 @@
 from time import time
 from math import sqrt
 from operator import itemgetter
-from itertools import takewhile
 from random import seed, randint
+from bisect import bisect_left as bsearch
 
 seed(123)
-M, N = 2500, 2500
-points = [(randint(-M, M), randint(-M, M)) for _ in range(N)]
+M, N = 100000000, 10000000
+points = [[randint(-M, M), randint(-M, M), 0] for _ in range(N)]
 Vx = sorted(points, key=itemgetter(0))
+Vy = sorted(points, key=itemgetter(1))
 
 
 def dist(a, b):
-	xa, ya = a
-	xb, yb = b
+	xa, ya, _ = a
+	xb, yb, _ = b
 	return sqrt((xa - xb) ** 2 + (ya - yb) ** 2)
 
 
-def d_and_d(S):
-	if len(S) < 2:
-		return (None, None), float('inf')
-	elif len(S) == 2:
-		return tuple(S), dist(*S)
+def d_and_d(Vx, Vy):
+	if len(Vx) < 2:
+		return [None, None, 0], float('inf')
+	elif len(Vx) == 2:
+		return Vx, dist(*Vx)
 
-	im = len(S) // 2
-	xm = S[im][0]
-	S1 = S[:im]
-	S2 = S[im:]
+	im = len(Vx) // 2
+	xm = Vx[im][0]
+	Vx1 = Vx[:im]
+	Vx2 = Vx[im:]
 
-	p1, d1 = d_and_d(S1)
-	p2, d2 = d_and_d(S2)
+	for p in Vx2: # mark each point in the right
+		p[-1] = 1
+	Vy1, Vy2 = [], []
+	for p in Vy: # divide between left and right
+		(Vy1, Vy2)[p[-1]].append(p)
+		p[-1] = 0 # remove mark
+
+	p1, d1 = d_and_d(Vx1, Vy1)
+	p2, d2 = d_and_d(Vx2, Vy2)
 	delta, pair = (d1, p1) if d1 < d2 else (d2, p2)
 
-	# TODO: Way to remove this sorting:
-	vy1F = list(takewhile(lambda p: p[0] > xm - delta, reversed(S1)))
-	vy1F.sort(key=itemgetter(1))
-	vy2F = list(takewhile(lambda p: p[0] < xm + delta, S2))
-	vy2F.sort(key=itemgetter(1))
+	# find strip bounds:
+	auxVx = [x[0] for x in Vx]
+	i = bsearch(auxVx, xm - delta)
+	j = bsearch(auxVx, xm + delta)
 
-	i = 0
-	for p in vy1F:
-		while i < len(vy2F) and p[1] > vy2F[i][1]:
-			i += 1
+	for p in Vx[max(0, i):j]: # mark points in the strip
+		p[-1] = 1
+	strip = [p for p in Vy if p[-1]]
 
-		cand = vy2F[max(0, i - 3):i + 3]
+	cand = strip[:6]
+	for i, p in enumerate(strip[3:-3]): # elevator like traversal
+		cand.append(strip[i + 3])
 		for q in cand:
+			q[-1] = 0 # remove mark
 			aux = dist(p, q)
-			if aux < delta:
+			if aux < delta and id(p) != id(q):
 				delta = aux
 				pair = (p, q)
+		cand.pop(0)
 
 	return pair, delta
 
@@ -64,9 +74,5 @@ def naive(S):
 	return pair, delta
 
 start = time()
-print(d_and_d(Vx))
-print('took %.6fs' % (time() - start))
-
-start = time()
-print(naive(Vx))
+print(d_and_d(Vx, Vy))
 print('took %.6fs' % (time() - start))
